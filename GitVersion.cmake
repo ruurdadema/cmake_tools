4412d12507613407061d50ad263f6e7c88f439fc
Ruurd Adema <ruurd@owllab.nl>
2022-08-26 08:57:39 +0200

    Improve error handling and reporting for GitVersion

--- GitVersion.cmake
+++ GitVersion.cmake
@@ -9,10 +9,11 @@ execute_process(
         OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-# Fallback if no tag was found
-if (GIT_DESCRIBE_VERSION STREQUAL "")
-    set(GIT_DESCRIBE_VERSION "v0.0.1-no-version")
-endif ()
+if (GIT_DESCRIBE_ERROR_CODE)
+    # Often it happens that no tag is available. Most CI systems only checkout the git history with a certain depth
+    # (ie. 50 commits). If the last tag is older than that, no tag is available and git describe will fail.
+    message(FATAL_ERROR "Git describe returned an error: ${GIT_DESCRIBE_VERSION}")
+endif()
 
 # Extract version components from git tags starting with 'v'
 string(REGEX MATCHALL "^v([0-9]+)\\.([0-9]+)\\.([0-9]+)(.*)$" VERSION_MATCH ${GIT_DESCRIBE_VERSION})
@@ -21,7 +22,8 @@ set(GIT_VERSION_MAJOR ${CMAKE_MATCH_1})
 set(GIT_VERSION_MINOR ${CMAKE_MATCH_2})
 set(GIT_VERSION_PATCH ${CMAKE_MATCH_3})
 
-message(STATUS "Git describe: ${GIT_DESCRIBE_VERSION}")
-message(STATUS "Version major: ${GIT_VERSION_MAJOR}")
-message(STATUS "Version minor: ${GIT_VERSION_MINOR}")
-message(STATUS "Version patch: ${GIT_VERSION_PATCH}")
+if (${GIT_VERSION_MAJOR} STREQUAL "" OR ${GIT_VERSION_MINOR} STREQUAL "" OR ${GIT_VERSION_PATCH} STREQUAL "")
+    message(FATAL_ERROR "No version info available")
+endif()
+
+message(STATUS "Git describe: ${GIT_DESCRIBE_VERSION}. Version major: ${GIT_VERSION_MAJOR}, version minor: ${GIT_VERSION_MINOR}, version patch: ${GIT_VERSION_PATCH}")
